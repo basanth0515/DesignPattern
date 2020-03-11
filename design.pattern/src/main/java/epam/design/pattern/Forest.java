0e5a2e32d899c6e1f4a3ba048492f2e1ec4156fa
basanth0515 <17pa1a1236@vishnu.edu.in>
2020-03-11 21:30:00 +0530

    commit

---
+++ design.pattern/src/main/java/epam/design/pattern/Forest.java
@@ -0,0 +1,30 @@
+package epam.design.pattern;
+
+import javax.swing.*;
+import epam.design.pattern.Tree;
+import epam.design.pattern.TreeFactory;
+import epam.design.pattern.TreeType;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Forest extends JFrame {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private List<Tree> trees = new ArrayList<>();
+
+    public void plantTree(int x, int y, String name, Color color, String otherTreeData) {
+        TreeType type = TreeFactory.getTreeType(name, color, otherTreeData);
+        Tree tree = new Tree(x, y, type);
+        trees.add(tree);
+    }
+
+    public void paint(Graphics graphics) {
+        for (Tree tree : trees) {
+            tree.draw(graphics);
+        }
+    }
+}
---
+++ design.pattern/src/main/java/epam/design/pattern/Singleton.java
@@ -0,0 +1,17 @@
+package epam.design.pattern;
+
+public class Singleton {
+     
+    private static Singleton myObj;
+    
+    public static Singleton getInstance(){
+        if(myObj == null){
+            myObj = new Singleton();
+        }
+        return myObj;
+    }
+     
+    public void getSomeThing(){
+        System.out.println("I am Singleton Design Pattern....");
+    }
+}
